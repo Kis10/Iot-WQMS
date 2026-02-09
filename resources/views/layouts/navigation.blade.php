eda1a512df3328f625fc0b6327629d3f19fd2229
Kirstine Sanchez <Kis10>
2026-02-10 01:11:00 +0800

    Remove chat link from navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,9 +26,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Refresh Button -->
                 @if(request()->routeIs('dashboard'))
-                    <x-nav-link :href="route('chat.page')" :active="request()->routeIs('chat.page')">
-                        Chat
-                    </x-nav-link>
+
                     <button id="refreshDashboard" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 me-3">
                         <svg class="w-4 h-4 me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
