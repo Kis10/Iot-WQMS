189e4be95736bbe5e8fb65ae2cf18d5a6e3bfe05
Kirstine Sanchez <Kis10>
2026-02-15 22:44:27 +0800

    Fix Approvals Table: Remove overflow constraints and implement floating action menus

--- resources/views/users/approvals.blade.php
+++ resources/views/users/approvals.blade.php
@@ -18,7 +18,7 @@
                     <h3 class="text-lg font-medium text-gray-900 mb-4">Users Waiting for Approval</h3>
                     
                     @if($pendingUsers->count() > 0)
-                        <div class="overflow-x-auto">
+                        <div>
                             <table class="min-w-full divide-y divide-gray-200">
                                 <thead class="bg-gray-50">
                                     <tr>
@@ -46,25 +46,25 @@
                                                 {{ $user->created_at->diffForHumans() }}
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                                                <div class="relative inline-block text-left" x-data="{ open: false }" @click.outside="open = false">
-                                                    <button @click="open = !open" type="button" class="inline-flex justify-center w-full rounded-full border border-gray-300 shadow-sm px-2 py-1 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                                <div class="relative inline-block text-left" @click.outside="openMenuId = null">
+                                                    <button @click="openMenuId = (openMenuId === {{ $user->id }} ? null : {{ $user->id }})" type="button" class="inline-flex justify-center w-full rounded-full border border-gray-300 shadow-sm px-2 py-1 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none" id="menu-button" aria-expanded="true" aria-haspopup="true">
                                                         <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                             <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
                                                         </svg>
                                                     </button>
 
-                                                    <div x-show="open" 
+                                                    <div x-show="openMenuId === {{ $user->id }}" 
                                                          x-transition:enter="transition ease-out duration-100"
                                                          x-transition:enter-start="transform opacity-0 scale-95"
                                                          x-transition:enter-end="transform opacity-100 scale-100"
                                                          x-transition:leave="transition ease-in duration-75"
                                                          x-transition:leave-start="transform opacity-100 scale-100"
                                                          x-transition:leave-end="transform opacity-0 scale-95"
-                                                         class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-20 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" style="display: none;">
+                                                         class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" style="display: none;">
                                                         <div class="py-1" role="none">
-                                                            <a href="#" @click.prevent="open = false; showDetails('{{ $user->name }}', '{{ $user->email }}', '{{ $user->created_at->format('M d, Y h:i A') }}')" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">Details</a>
-                                                            <a href="#" @click.prevent="open = false; processApprove({{ $user->id }})" class="text-green-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">Approve</a>
-                                                            <a href="#" @click.prevent="open = false; processDeny({{ $user->id }})" class="text-red-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">Deny</a>
+                                                            <a href="#" @click.prevent="openMenuId = null; showDetails('{{ $user->name }}', '{{ $user->email }}', '{{ $user->created_at->format('M d, Y h:i A') }}')" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">Details</a>
+                                                            <a href="#" @click.prevent="openMenuId = null; processApprove({{ $user->id }})" class="text-green-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">Approve</a>
+                                                            <a href="#" @click.prevent="openMenuId = null; processDeny({{ $user->id }})" class="text-red-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1">Deny</a>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -154,6 +154,7 @@
             isLoading: false,
             showNotification: false,
             notificationMessage: '',
+            openMenuId: null,
             details: { name: '', email: '', date: '' },
 
             showDetails(name, email, date) {
